787c4d9735a01e365ec3342a90ea1a2f3f384a5e
wyzip <2362086452@qq.com>
2021-11-15 16:14:45 +0800

    no message

--- _test.js
+++ _test.js
@@ -2,9 +2,22 @@ const webSocketServer = require('./index');
 const server = new webSocketServer({port: 3000});
 
 server.on('connection', (on, use) => {
+
+  use((res, ws, req, wss, next) => {
+    if(res.type === 'connect'){
+      return ws.send(req.query.name + '进入房间')
+    }
+    next();
+  })
   on('/test', (res, ws, req, wss) => {
+    res.content = res.data.toString();
     console.log(res);
-    console.log(res.data.toString());
-    ws.send('test')
+    ws.send('你好, ' + req.query.name)
+  })
+
+  on('/test2', (res, ws, req, wss) => {
+    res.content = res.data.toString();
+    console.log(res);
+    ws.send('你好, ' + req.query.name)
   })
 })

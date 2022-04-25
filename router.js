0e60be2281aeabedb79480e0ba866d4b036caa80
wyzip <2362086452@qq.com>
2022-04-25 15:16:46 +0800

    next问题修复

--- router.js
+++ router.js
@@ -1,12 +1,20 @@
 const router = {};
 
 const use = {
-  __global: []
+  __global: [],
+  __globalLast: null
 };
 
+Object.defineProperty(use, '__globalLast', {
+  get() {
+    const globalLength = use['__global'].length
+    return use['__global'][globalLength - 1]
+  }
+})
+
 exports.use = (key, cb) => {
-  if(typeof key !== 'string') return use.__global.push(cb);
-  if(!use[key])use[key] = cb;
+  if(typeof key === 'function') return use.__global.push(key);
+  use[key] = cb;
 }
 
 exports.on = (key, cb) => router[key] = cb;
@@ -17,14 +25,14 @@ exports.run = (key, res, ws, req, wss) => {
   for(let _use of use.__global) {
     _use(res, ws, req, wss, next);
     if (!_continue) break;
-    _continue = false;
+    if(_use !== use.__globalLast)_continue = false;
   }
   if(!use.__global.length) _continue = true;
   if(!_continue) return;
   if(use[key]) {
     _continue = false;
     use[key](res, ws, req, wss, next);
-  }
+  } 
   if(!_continue) return;
   router[key] && router[key](res, ws, req, wss);
 }
